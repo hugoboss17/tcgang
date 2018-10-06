35d7437e097bbbaa4b09d7cb45306a9165965dd8
Hugo Rodrigues <hugodfrodrigues@gmail.com>
2018-10-06 14:24:06 +0100

    Seeded Yugioh game - it will be the first one getting values of the cards

---
+++ database/seeds/GameTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class GameTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('games')->insert([
+            'name' => 'Yu-Gi-Oh!'
+        ]);
+    }
+}
